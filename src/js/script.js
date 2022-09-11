f422e618a6d196fcb3cad72de3166d26bf21f0f2
lucashtml6 <lucassouzarhcp9@gmail.com>
2022-09-11 19:12:18 -0300

    feat: adicionando verificação caso não houver nenhuma opção selecionada

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+// Seleção
+const form = document.getElementById("form");
+const inputTamanho = document.getElementById("tamanho");
+const opcoes = document.querySelectorAll(".opcoes");
+const inputSenhaGerada = document.getElementById("senha-gerada");
+const mensagemDeErro = document.getElementById("mensagem-de-erro");
+
+const letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+const letrasMaiusculas = letrasMinusculas.toUpperCase();
+const numeros = "0123456789";
+const simbolos = "!@#$*";
+
+// Funções
+const verificarOpcoesMarcadas = (opcoes) => {
+  if (
+    !opcoes[0].checked &&
+    !opcoes[1].checked &&
+    !opcoes[2].checked &&
+    !opcoes[3].checked
+  ) {
+    return true;
+  }
+};
+
+// Eventos
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (verificarOpcoesMarcadas(opcoes)) {
+    mensagemDeErro.classList.remove("hidden");
+    inputSenhaGerada.value = "";
+  } else {
+    mensagemDeErro.classList.add("hidden");
+    inputSenhaGerada.value = "senha";
+  }
+
+  
+});
